0134937d05dfb980a237ac72fb076a3160d62065
Maksim Inyushkin <58373833+Maximus231@users.noreply.github.com>
2024-04-10 18:14:02 +0300

    Add files via upload

--- lang.py
+++ lang.py
@@ -1,6 +1,9 @@
 from googletrans import Translator
-import requests
 import json
+import my_logging as lg
+import requests
+import nltk
+import re
 
 def obtain_substring_with_extra_word(original_string, a, b):
     # Split the original string into words
@@ -34,11 +37,58 @@ def obtain_substring_with_extra_word(original_string, a, b):
     return new_substring
 
 
+def insert_spaces_between_sentences(text):
+	# List of punctuation marks that usually denote the end of a sentence
+	punctuation_marks = ['.', '!', '?']
+
+	# Initialize an empty string to store the result
+	result = ''
+
+	# Iterate through each character in the text
+	for i, char in enumerate(text):
+		# Add the current character to the result
+		result += char
+
+		# If the current character is a punctuation mark and the next character is not a space
+		if char in punctuation_marks and i + 1 < len(text) and text[i + 1] != ' ':
+			# Add a space after the punctuation mark
+			result += ' '
+
+	return result
+
+
+def remove_telegram_emojis(text):
+	# Regular expression pattern to match Telegram emojis
+	emoji_pattern = re.compile("[\U0001F600-\U0001F64F"  # emoticons
+							   "\U0001F300-\U0001F5FF"  # symbols & pictographs
+							   "\U0001F680-\U0001F6FF"  # transport & map symbols
+							   "\U0001F700-\U0001F77F"  # alchemical symbols
+							   "\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
+							   "\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
+							   "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+							   "\U0001FA00-\U0001FA6F"  # Chess Symbols
+							   "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
+							   "\U00002702-\U000027B0"  # Dingbats
+							   "\U000024C2-\U0001F251"
+							   "]+", flags=re.UNICODE)
+
+	# Remove Telegram emojis from the text
+	text_without_emojis = emoji_pattern.sub(r'', text)
+
+	return text_without_emojis
+
+
 def translate_text(text, target_language='en'):
-    translator = Translator()
-    translated_text = translator.translate(text, dest=target_language)
-    res = translated_text.text
-    return res
+	#nltk.download('punkt')
+	lg.log_message("INFO", "Translation. Message to translate: " + text)
+	translator = Translator()
+	translated_text = translator.translate(text, dest=target_language)
+	# Split the translated text into sentences
+	#translated_sentences = nltk.sent_tokenize(translated_text.text)
+	# Join the translated sentences with spaces
+	translated_text_with_spaces = insert_spaces_between_sentences(translated_text.text)
+	#translated_text_with_spaces = ' '.join(translated_sentences)
+	return translated_text_with_spaces
 
 
 def highlight_message(message, mistakes):
@@ -104,11 +154,14 @@ def ginger_parser(ginger_response="", initial_message=""):  # input: ginger's re
 
 
 def check_mistakes(message, test_mode=True, show_descriptions=False):
+	lg.log_message("INFO", "Start working with MESSAGE:" + message)
 	message = remove_tags(message)
 	if not test_mode:
+		lg.log_message("TEST", "LIVE MODE (TEST = FALSE)")
 		url = "https://ginger4.p.rapidapi.com/correction"
 		querystring = {"lang": "US", "generateRecommendations": "true", "flagInfomralLanguage": "true"}
 		payload = message
+		#main.log.log_message("INFO", "MESSAGE (tags removed):"+message)
 		headers = {
 			"content-type": "text/plain",
 			"Content-Type": "text/plain",
@@ -116,14 +169,20 @@ def check_mistakes(message, test_mode=True, show_descriptions=False):
 			"X-RapidAPI-Key": "3fcd771099msh8c3893b5292a80ep171395jsn7b52a76a079d",
 			"X-RapidAPI-Host": "ginger4.p.rapidapi.com"
 		}
-		ginger_resp = requests.post(url, data=payload, headers=headers, params=querystring)
+
+		try:
+			lg.log_message("PAYED", "Payed function has started. SERVICE: Ginger")
+			ginger_resp = requests.post(url, data=payload, headers=headers, params=querystring)
+			ginger_resp.raise_for_status()  # Raise an exception for HTTP errors
+		except requests.RequestException as e:
+			lg.log_message("ERROR", "Can't receive an answer from Ginger"+e)
+			lg.log_message("ERROR DATA", "Request:" + message)
+			lg.log_message("ERROR DATA", "Ginger response:" + ginger_resp.text)
+			print('Problem in lang.py, line 133')
 		initial_message = message
 		print("Called payed function!")
-		print("REQUEST")
-		print(initial_message)
-		print("RESPONSE")
-		print(ginger_resp.text)
 	else:
+		lg.log_message("TEST?", "TRUE, TEST MODE")
 		file_path = 'data/ginger_response_example.txt'
 		with open(file_path, 'r') as file:
 			ginger_resp = json.load(file)
---
+++ my_logging.py
@@ -0,0 +1,20 @@
+import csv
+import datetime
+
+import lang
+
+
+def log_message(message_type, data):
+    # Create the directory if it doesn't exist
+    file_name = 'logs/audit_log_april_2024.csv'
+
+    # Get the current time
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # Write the log entry to the CSV file
+    with open(file_name, "a", newline="") as file:
+        try:
+            writer = csv.writer(file)
+            writer.writerow([current_time, message_type, data])
+        except:
+            writer.writerow([current_time, message_type, "Cant log message with emoji. Message w/o emoji: " + lang.remove_telegram_emojis(data)])
+            print("Problems with update log!")
